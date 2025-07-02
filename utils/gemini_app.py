45b9f497a752baa518a749ad16e0c24c92301770
Narayanan K <119158621+NarayananK05@users.noreply.github.com>
2025-07-02 13:16:53 +0530

    Minor changes

--- utils/gemini_app.py
+++ utils/gemini_app.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAu6ZBVcwq6L85YHFdQBzn46xCs5f4iPAE")
+genai.configure(api_key="AIzaSyCrtz8JR6AXCzJ_5XfJnDc3kO-a2YKI15U")
 
 def gemini_chat(prompt, system="You are a helpful assistant."):
     model = genai.GenerativeModel(model_name="models/gemini-1.5-flash", system_instruction=system)
